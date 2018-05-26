e9cd7e8bae5f4caf13b92866cf1585d3aec24d15
Phil <philippe.godbout.1@ens.etsmtl.ca>
2018-05-26 18:21:45 -0400

    Modification des models Courant, Compte, Credit, Transaction et TypeTransaction

--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Adresse(models.Model):
     no_civique = models.CharField(max_length=30)
@@ -22,34 +21,51 @@ class Client(models.Model):
     numero_entreprise = models.CharField(max_length=50)
 
 
-class Courant(models.Model):
-    first_name = models.CharField(max_length=30)
-
-
-class Credit(models.Model):
-    limite = models.DecimalField(max_digits=10, decimal_places=2)
-
-
-class Compte(models.Model):
-    solde = models.DecimalField(max_digits=10, decimal_places=2)
-    num_compte = models.CharField(max_length=8)
-    date_ouverture = models.DateTimeField(auto_now_add=True)
-    date_fermeture = models.DateTimeField()
-
-
 class CarteCredit(models.Model):
+    pk_carte_Credit_id = models.AutoField(primary_key=True)
     nom_titulaire = models.CharField(max_length=100)
-    num_carte = models.CharField(max_length=100)
+    num_carte = models.CharField(max_length=100, unique=True)
     annee_expiration = models.CharField(max_length=2)
     mois_expiration = models.CharField(max_length=2)
-    cvv = models.CharField(max_length=3)
+    cvv = models.CharField(max_length=512)
     date_emission = models.DateTimeField(auto_now_add=True)
 
 
+class Compte(models.Model):
+    pk_compte_id = models.AutoField(primary_key=True)
+    num_compte = models.CharField(max_length=8, unique=True)
+    solde = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ouverture = models.DateTimeField(auto_now_add=True)
+    date_fermeture = models.DateTimeField(null=True)
+
+
+class Courant(Compte):
+    pk_compte_id = models.AutoField(primary_key=True)
+    fk_client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    fk_compte_id = models.ForeignKey(Compte, on_delete=models.CASCADE)
+
+
+class Credit(Compte):
+    pk_compte_id = models.AutoField(primary_key=True)
+    fk_compte_id = models.ForeignKey(Compte, on_delete=models.CASCADE)
+    fk_client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    fk_carte_Credit_id = models.ForeignKey(CarteCredit, on_delete=models.CASCADE)
+    limite = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class TypeTransaction(models.Model):
+    pk_type_transaction_id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=50)
+
 
 class Transaction(models.Model):
-    montant = models.DecimalField(max_digits=10, decimal_places=2)
+    pk_transaction_id = models.AutoField(primary_key=True)
+    fk_type_transaction_id = models.ForeignKey(TypeTransaction, on_delete=models.DO_NOTHING)
+    fk_compte_id = models.ForeignKey(Compte, on_delete=models.DO_NOTHING)
+    fk_transaction_id = models.ForeignKey('self', on_delete=models.DO_NOTHING)
     date_debut = models.DateTimeField(auto_now_add=True)
-    date_fin = models.DateTimeField()
+    date_fin = models.DateTimeField(null=True)
+    montant = models.DecimalField(max_digits=10, decimal_places=2)
+    solde_avant = models.DecimalField(max_digits=10, decimal_places=2)
+    solde_apres = models.DecimalField(max_digits=10, decimal_places=2)
     etat = models.CharField(max_length=3)
-    
