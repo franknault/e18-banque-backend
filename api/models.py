2ef0b06ef65973138c03dce6b28fc5c93d1923fa
Francis Nault <francis.nault.1@ens.etsmtl.ca>
2018-05-26 18:18:51 -0400

    Ajout des modèles Adresse et Client

--- api/models.py
+++ api/models.py
@@ -2,24 +2,67 @@ from django.db import models
 
 # Create your models here.
 
+
 class Adresse(models.Model):
+    pk_adresse = models.AutoField(primary_key=True)
     no_civique = models.CharField(max_length=30)
     nom_rue = models.CharField(max_length=50)
     code_postal = models.CharField(max_length=6)
     ville = models.CharField(max_length=50)
     pays = models.CharField(max_length=50)
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+
+    @property
+    def full_address(self):
+        "Return the full address"
+        return '%s %s, %s, %s, %s' % (self.no_civique, self.nom_rue, self.code_postal, self.ville, self.pays)
+
+    class Meta:
+        db_table = 'adresse'
+        verbose_name = 'adresse'
+        verbose_name_plural = 'adresses'
 
 
 class Client(models.Model):
+    HOMME = 'H'
+    FEMME = 'F'
+    AUCUN = 'A'
+    SEXE_CHOICES = (
+        (HOMME, 'Homme'),
+        (FEMME, 'Femme'),
+        (AUCUN, 'Non défini'),
+    )
+
+    ENTREPRISE = 'E'
+    PARTICULIER = 'P'
+    TYPE_CHOICES = (
+        (ENTREPRISE, 'Entreprise'),
+        (PARTICULIER, 'Particulier'),
+    )
+
+    pk_client = models.AutoField(primary_key=True)
     courriel = models.EmailField()
     telephone = models.CharField(max_length=11)
-    type = models.CharField(max_length=1)
-    nom_particulier = models.CharField(max_length=50)
-    prenom_particulier = models.CharField(max_length=50)
-    sexe = models.CharField(max_length=1)
-    date_naissance = models.DateField()
-    nom_entreprise = models.CharField(max_length=50)
-    numero_entreprise = models.CharField(max_length=50)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    nom_particulier = models.CharField(max_length=50, null=True, blank=True)
+    prenom_particulier = models.CharField(max_length=50, null=True, blank=True)
+    sexe = models.CharField(max_length=1, choices=SEXE_CHOICES, null=True, blank=True)
+    date_naissance = models.DateField(null=True, blank=True)
+    nom_entreprise = models.CharField(max_length=50, null=True, blank=True)
+    numero_entreprise = models.CharField(max_length=50, null=True, blank=True)
+
+    @property
+    def full_name(self):
+        "Return the full name"
+        if self.nom_particulier and self.prenom_particulier:
+            return '%s %s' % (self.prenom_particulier, self.nom_particulier)
+        elif self.nom_entreprise and self.numero_entreprise:
+            return '%s, %s' % (self.nom_entreprise, self.numero_entreprise)
+
+    class Meta:
+        db_table = 'client'
+        verbose_name = 'client'
+        verbose_name_plural = 'clients'
 
 
 class Courant(models.Model):
