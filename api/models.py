49930acbba85f7e590ff990cc23b9909facec622
Phil <philippe.godbout.1@ens.etsmtl.ca>
2018-06-04 23:12:28 -0400

    Ajout des modèles Administrateur, Session et InfoAuthentification. Modification des nom d'attributs afin qu'ils soient en minuscules et contienne des '_' entre les mots

--- api/models.py
+++ api/models.py
@@ -1,24 +1,16 @@
 from django.db import models
 
 
-class Adresse(models.Model):
-    pk_adresse = models.AutoField(primary_key=True)
-    no_civique = models.CharField(max_length=30)
-    nom_rue = models.CharField(max_length=50)
-    code_postal = models.CharField(max_length=6)
-    ville = models.CharField(max_length=50)
-    pays = models.CharField(max_length=50)
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+class InfoAuthentification(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom_usager: models.CharField(max_length=20, unique=True)
+    mdp: models.CharField(max_length=512)
+    courriel = models.EmailField()
 
-    @property
-    def full_address(self):
-        "Return the full address"
-        return '%s %s, %s, %s, %s' % (self.no_civique, self.nom_rue, self.code_postal, self.ville, self.pays)
 
-    class Meta:
-        db_table = 'adresse'
-        verbose_name = 'adresse'
-        verbose_name_plural = 'adresses'
+class Administrateur(models.Model):
+    id = models.AutoField(primary_key=True)
+    info_authentification = models.ForeignKey(InfoAuthentification, on_delete=models.CASCADE)
 
 
 class Client(models.Model):
@@ -38,8 +30,8 @@ class Client(models.Model):
         (PARTICULIER, 'Particulier'),
     )
 
-    pk_client = models.AutoField(primary_key=True)
-    courriel = models.EmailField()
+    id = models.AutoField(primary_key=True)
+    info_authentification = models.ForeignKey(InfoAuthentification, on_delete=models.CASCADE)
     telephone = models.CharField(max_length=11)
     type = models.CharField(max_length=1, choices=TYPE_CHOICES)
     nom_particulier = models.CharField(max_length=50, null=True, blank=True)
@@ -52,10 +44,10 @@ class Client(models.Model):
     @property
     def full_name(self):
         "Return the full name"
-        if self.nom_particulier and self.prenom_particulier:
-            return '%s %s' % (self.prenom_particulier, self.nom_particulier)
-        elif self.nom_entreprise and self.numero_entreprise:
-            return '%s, %s' % (self.nom_entreprise, self.numero_entreprise)
+        if self.NomParticulier and self.PrenomParticulier:
+            return '%s %s' % (self.PrenomParticulier, self.NomParticulier)
+        elif self.NomEntreprise and self.NumeroEntreprise:
+            return '%s, %s' % (self.NomEntreprise, self.NumeroEntreprise)
 
     class Meta:
         db_table = 'client'
@@ -63,8 +55,36 @@ class Client(models.Model):
         verbose_name_plural = 'clients'
 
 
+class Adresse(models.Model):
+    id = models.AutoField(primary_key=True)
+    no_civique = models.CharField(max_length=30)
+    nom_rue = models.CharField(max_length=50)
+    code_postal = models.CharField(max_length=6)
+    ville = models.CharField(max_length=100)
+    pays = models.CharField(max_length=100)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    @property
+    def full_address(self):
+        "Return the full address"
+        return '%s %s, %s, %s, %s' % (self.NoCivique, self.NomRue, self.CodePostal, self.Ville, self.Pays)
+
+    class Meta:
+        db_table = 'adresse'
+        verbose_name = 'adresse'
+        verbose_name_plural = 'adresses'
+
+
+class Session(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    est_connecte: models.BooleanField
+    est_valide: models.BooleanField
+    token: models.CharField(max_length=512, null=True, blank=True)
+
+
 class Compte(models.Model):
-    pk_compte = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     num_compte = models.CharField(max_length=8, unique=True)
     solde = models.DecimalField(max_digits=10, decimal_places=2)
     date_ouverture = models.DateTimeField(auto_now_add=True)
@@ -75,20 +95,20 @@ class Compte(models.Model):
 
 
 class Courant(Compte):
-    pk_courant = models.AutoField(primary_key=True)
-    fk_client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    courant_id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'courant'
 
 
 class CarteCredit(models.Model):
-    pk_carte_credit = models.AutoField(primary_key=True)
-    nom_titulaire = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    nom_titulaire = models.CharField(max_length=200)
     num_carte = models.CharField(max_length=100, unique=True)
     annee_expiration = models.CharField(max_length=2)
     mois_expiration = models.CharField(max_length=2)
-    cvv = models.CharField(max_length=512)
+    cvv = models.CharField(max_length=255)
     date_emission = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -96,9 +116,9 @@ class CarteCredit(models.Model):
 
 
 class Credit(Compte):
-    pk_credit = models.AutoField(primary_key=True)
-    fk_client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    fk_carte_credit = models.ForeignKey(CarteCredit, on_delete=models.CASCADE)
+    credit_id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    carte_credit = models.ForeignKey(CarteCredit, on_delete=models.CASCADE)
     limite = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
@@ -106,8 +126,19 @@ class Credit(Compte):
 
 
 class TypeTransaction(models.Model):
-    pk_type_transaction_id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=50)
+    VIREMENTDEBITDEBIT = 'VMT'
+    PAIEMENTDEDEBITACREDIT = 'PMT'
+    ACHATDEBITACREDIT = 'ACT'
+    REMBOURSEMENTCREDITACREDIT = 'RBT'
+    TYPE_CHOICES = (
+        (VIREMENTDEBITDEBIT, 'Virement debit-debit'),
+        (PAIEMENTDEDEBITACREDIT, 'Paiement debit-credit'),
+        (ACHATDEBITACREDIT, 'Achat debit-credit'),
+        (REMBOURSEMENTCREDITACREDIT, 'Remboursement credit-credit'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     description = models.TextField()
 
     class Meta:
@@ -123,10 +154,11 @@ class Transaction(models.Model):
         (REFUSE, 'Refusée'),
         (GELE, 'Gelée'),
     )
-    pk_transaction = models.AutoField(primary_key=True)
-    fk_type_transaction = models.ForeignKey(TypeTransaction, on_delete=models.DO_NOTHING)
-    fk_compte = models.ForeignKey(Compte, on_delete=models.DO_NOTHING)
-    fk_transaction = models.ForeignKey('self', on_delete=models.DO_NOTHING)
+
+    id = models.AutoField(primary_key=True)
+    type_transaction = models.ForeignKey(TypeTransaction, on_delete=models.DO_NOTHING)
+    compte = models.ForeignKey(Compte, on_delete=models.DO_NOTHING)
+    type_transaction = models.ForeignKey('self', on_delete=models.DO_NOTHING)
     date_debut = models.DateTimeField(auto_now_add=True)
     date_fin = models.DateTimeField(null=True)
     montant = models.DecimalField(max_digits=10, decimal_places=2)
