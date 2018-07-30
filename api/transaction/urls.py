5472ae0d04d9e0f3bbcf7449eaf3690535c9cb64
jvrbn <javierjr.beltran@gmail.com>
2018-07-30 15:25:31 -0400

    Creation serializer pour obtenir transaction de courant et credit separement

--- api/transaction/urls.py
+++ api/transaction/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('client/transaction', views.TransactionCompte.as_view()),
+    path('client/transaction/courant', views.TransactionCompteCourant.as_view()),
+    path('client/transaction/credit', views.TransactionCompteCredit.as_view()),
     path('admin/transaction', views.TransactionsList.as_view()),
     path('admin/transaction/<int:pk>', views.TransactionId.as_view())
 ]
